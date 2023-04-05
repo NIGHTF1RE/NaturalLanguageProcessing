d2eca22b62ccfb693a574a8fc909b20ac8808719
NIGHTF1RE <117327369+NIGHTF1RE@users.noreply.github.com>
2023-04-04 21:26:08 -0500

    Update QCUtilityFunctions.py
    Did some local QA instead of just writing functions here.

--- GenerativeAI/QCUtilityFunctions.py
+++ GenerativeAI/QCUtilityFunctions.py
@@ -27,7 +27,7 @@ def NGramRepeatedFraction(String, n):
     String = String.translate(str.maketrans("","",string.punctuation))
     
     WordList = re.split('\s+',String)
-    WordList = [item for item in WordList if len(item)>0]
+    WordList = [item.lower() for item in WordList if len(item)>0]
     NGrams = [' '.join(WordList[i:i+n]) for i in range(len(WordList)-n)]
     
     NGramCount = Counter(NGrams)
@@ -35,7 +35,12 @@ def NGramRepeatedFraction(String, n):
     
     return sum([i for i in a if i>1])/sum(a)
 
-def SelfSimilarity(String, WordsForRemoval=[], Threshold=0.7, CrossEncode=False):
+
+# This function takes a string and compares the unique pair to each other. It can help
+# note patterns of semantic repetition that may be missed by repeated ngrams (aka,
+# you couldn't beat it with just a thesaurus). However, it is significantly slower and
+# has a number of othe quirks, many of which are explained within the function.
+def SelfSimilarity(String, WordsForRemoval=[], Threshold=0.7, CrossEncode=False, PrintScores=False):
     
     # Allows for the removal of certain words that may dominate the embedding and cause
     # sentences that share the words to be closer together than desired or sentences that
@@ -43,15 +48,29 @@ def SelfSimilarity(String, WordsForRemoval=[], Threshold=0.7, CrossEncode=False)
     # about things to do in Paris, the presence or absence of the word Paris would strongly
     # influence the embedding.
     String = re.sub('|'.join(WordsForRemoval),'',String)
-    String = re.sub('  ',' ',String)
+    String = re.sub(' +',' ',String)
     
     # Quick and dirty sentnece splits on likely tokens. Suffers from certain titles,
     # abbreviations, and other relatively uncommon grammar and syntax. If these are 
-    # expected to be common, it is likely bett
+    # expected to be common, it is likely better to use a more thorough method of
+    # splitting, such as a spaCy sentencizer.
+    
     Sents = re.split('\.|!|;|\n|:',String)
+    
+    # Punctuation has a small but consistent effect that causes sentences to 
+    # score as more related when using embedding and cosine similarity. This
+    # effect does not appear as strong for cross-encoding.
     #Sents = [s.translate(str.maketrans('','',string.punctuation)) for s in Sents]
+    
+    # Again, a quick and dirty way to ensure that sentences are meaningful. Similar
+    # to the sentnence splitting, doing this cutoff by word might be more effective.
+    # However, that is really only a problem if you are using very large words that
+    # let you hit the 25 character threshold without saying much. It is also worth
+    # noting that what exactly this threshold should be likely largely depends on
+    # the content.
     Sents = [re.sub('\A\W*','',s) for s in Sents if len(s)>25]
     
+    # Initialize these for creating a dataframe later    
     ScoreList = []
     Sent1List = []
     Sent2List = []
@@ -88,13 +107,15 @@ def SelfSimilarity(String, WordsForRemoval=[], Threshold=0.7, CrossEncode=False)
         
     data = pd.DataFrame({'Scores':ScoreList, 'Sent1':Sent1List, 'Sent2':Sent2List})
 
-    data = data.sort_values('Score', ascending=False)
-    for ind in data.index:
-        if data.at[ind,'Scores']>Threshold:
-            if abs(data.at[ind,'Senti1'] - data.at[ind,'Senti2'])>.0:
-                print(data.at[ind,'Scores']*abs(data.at[ind,'Senti1'] - data.at[ind,'Senti2']))
+    data = data.sort_values('Scores', ascending=False)
+    
+    # Useful to simply see the the most similar sentences without passing them
+    # to some other code.
+    if PrintScores:
+        for ind in data.index:
+            if data.at[ind,'Scores']>Threshold:
                 print(data.at[ind,'Scores'])
-                print(data.at[ind,'Senti1'], data.at[ind,'Sent1'])
-                print(data.at[ind,'Senti2'], data.at[ind,'Sent2'])
+                print(data.at[ind,'Sent1'])
+                print(data.at[ind,'Sent2'])
                 print()
     return data
